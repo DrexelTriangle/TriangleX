eeeffae7d225ff547925949a169c032ec6f4ce9c
Mike Avena (Triangle) <mike.avena@thetriangle.org>
2016-10-05 13:53:19 +0000

    Updated gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,13 @@
 var gulp = require('gulp'),
 	sass = require('gulp-ruby-sass'),
-	autoprefixer = require('gulp-autoprefixer'),
-	cssnano = require('gulp-cssnano'),
 	jshint = require('gulp-jshint'),
-	uglify = require('gulp-uglify'),
-	imagemin = require('gulp-imagemin'),
-	rename = require('gulp-rename'),
-	concat = require('gulp-concat'),
-	notify = require('gulp-notify'),
-	cache = require('gulp-cache'),
-	livereload = require('gulp-livereload'),
-	del = require('del');
+	uglify = require('gulp-uglify');
 
-gulp.task('default', function()
+gulp.task('default', ['sass']);
+
+gulp.task('sass', function()
 {
-	return sass('scss/style.scss', { style: 'expanded' })
-	.pipe(autoprefixer('last 2 version'))
-	.pipe(gulp.dest('/'))
-	.pipe(rename({suffix: '.min'}))
-	.pipe(cssnano())
-	.pipe(gulp.dest('/'))
-	.pipe(notify({ message: 'Styles task complete' }));
+    return sass('scss/syle.scss')
+        .on('error', sass.logError)
+        .pipe(gulp.dest('./'));
 });
