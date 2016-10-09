05405bc26efe291e97cbeba6f75bb0f4bfd3d926
Mike Avena <michael.avena@gmail.com>
2016-10-08 21:18:09 -0400

    Removed watch from gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -3,16 +3,11 @@
 var gulp = require('gulp'),
 	sass = require('gulp-sass');
 
-gulp.task('default', ['sass:watch']);
+gulp.task('default', ['sass']);
 
 gulp.task('sass', function()
 {
-    return gulp.src('./scss/style.scss')
+    return gulp.src('./scss/*.scss')
 		.pipe(sass().on('error', sass.logError))
 		.pipe(gulp.dest('./'));
-});
-
-gulp.task('sass:watch', function ()
-{
-  gulp.watch('./scss/style.scss', ['sass']);
 });
