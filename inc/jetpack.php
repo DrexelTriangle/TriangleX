a516512df31bdf081ff9b07133ed0efc797b1b7c
Mike Avena <michael.avena@gmail.com>
2016-11-26 14:17:29 -0500

    Added function to remove ShareDaddy buttons from bottom of post; fixed formatting

--- inc/jetpack.php
+++ inc/jetpack.php
@@ -13,29 +13,42 @@
  * See: https://jetpack.com/support/infinite-scroll/
  * See: https://jetpack.com/support/responsive-videos/
  */
-function triangle_x_jetpack_setup() {
+function triangle_x_jetpack_setup()
+{
 	// Add theme support for Infinite Scroll.
-	add_theme_support( 'infinite-scroll', array(
+	add_theme_support('infinite-scroll', array(
 		'container' => 'main',
 		'render'    => 'triangle_x_infinite_scroll_render',
 		'footer'    => 'page',
-	) );
+	));
 
 	// Add theme support for Responsive Videos.
-	add_theme_support( 'jetpack-responsive-videos' );
+	add_theme_support('jetpack-responsive-videos');
 }
-add_action( 'after_setup_theme', 'triangle_x_jetpack_setup' );
+add_action('after_setup_theme', 'triangle_x_jetpack_setup');
 
-/**
- * Custom render function for Infinite Scroll.
- */
-function triangle_x_infinite_scroll_render() {
-	while ( have_posts() ) {
+// Custom render function for Infinite Scroll.
+function triangle_x_infinite_scroll_render()
+{
+	while (have_posts())
+	{
 		the_post();
-		if ( is_search() ) :
-			get_template_part( 'template-parts/content', 'search' );
-		else :
+		
+		if (is_search())
+			get_template_part('template-parts/content', 'search');
+		else
 			get_template_part( 'template-parts/content', get_post_format() );
-		endif;
 	}
 }
+
+// Removes ShareDaddy buttons from bottom of content posts.
+function jptweak_remove_share()
+{
+    remove_filter('the_content', 'sharing_display', 19);
+    remove_filter('the_excerpt', 'sharing_display', 19);
+    if (class_exists('Jetpack_Likes'))
+        remove_filter('the_content', array(Jetpack_Likes::init(), 'post_likes'), 30, 1);
+}
+add_action('loop_start', 'jptweak_remove_share');
+
+?>
