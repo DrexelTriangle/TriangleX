bf42f46c31f75febfadebd8ae62283b0af534c9d
Mike Avena <michael.avena@gmail.com>
2018-11-30 20:49:27 -0500

    Fixed bottom medium rectangle ad code

--- inc/openx/GlobalMediumRectangleBottom.php
+++ inc/openx/GlobalMediumRectangleBottom.php
@@ -1,13 +1,13 @@
 <!--/* OpenX Asynchronous JavaScript tag */-->
 
-<div id="538119720_INSERT_SLOT_ID_HERE" style="width:728px;height:90px;margin:0;padding:0">
-  <noscript><iframe id="bf0ba1060c" name="bf0ba1060c" src="//oncampusweb-d.openx.net/w/1.0/afr?auid=538119720&cb=INSERT_RANDOM_NUMBER_HERE" frameborder="0" scrolling="no" width="728" height="90"><a href="//oncampusweb-d.openx.net/w/1.0/rc?cs=bf0ba1060c&cb=INSERT_RANDOM_NUMBER_HERE" ><img src="//oncampusweb-d.openx.net/w/1.0/ai?auid=538119720&cs=bf0ba1060c&cb=INSERT_RANDOM_NUMBER_HERE" border="0" alt=""></a></iframe></noscript>
+<div id="538119722_INSERT_SLOT_ID_HERE" style="width:300px;height:250px;margin:0;padding:0">
+  <noscript><iframe id="a79c035e7a" name="a79c035e7a" src="//oncampusweb-d.openx.net/w/1.0/afr?auid=538119722&cb=INSERT_RANDOM_NUMBER_HERE" frameborder="0" scrolling="no" width="300" height="250"><a href="//oncampusweb-d.openx.net/w/1.0/rc?cs=a79c035e7a&cb=INSERT_RANDOM_NUMBER_HERE" ><img src="//oncampusweb-d.openx.net/w/1.0/ai?auid=538119722&cs=a79c035e7a&cb=INSERT_RANDOM_NUMBER_HERE" border="0" alt=""></a></iframe></noscript>
 </div>
 <script type="text/javascript">
   var OX_ads = OX_ads || [];
   OX_ads.push({
-     slot_id: "538119720_INSERT_SLOT_ID_HERE",
-     auid: "538119720"
+     slot_id: "538119722_INSERT_SLOT_ID_HERE",
+     auid: "538119722"
   });
 </script>
 
