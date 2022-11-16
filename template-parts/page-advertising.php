371ae2cbc9b5eab5547457c7dfa2c85333c052f2
triangleAdmin <79601735+triangleAdmin@users.noreply.github.com>
2022-11-16 18:35:02 -0500

    Update page-advertising.php

--- template-parts/page-advertising.php
+++ template-parts/page-advertising.php
@@ -13,10 +13,10 @@
 </div>
 
 <div class="generic-container">
-	<p>Can&rsquo;t see the PDF? Click <a href="http://files.thetriangle.org/rate-cards/ratecard.pdf">here</a> to download a copy.</p>
-	<iframe style="width: 100%; height: 11in;" src="http://files.thetriangle.org/rate-cards/ratecard.pdf" frameborder="0" seamless></iframe> 
-	<!-- <p> The Triangle offers a number of print and digital advertising packages to suit your needs. View our 
-		rate card <a href="https://app.flytedesk.com/v3/shop/the-triangle-a3d66669/97b07f0a-4db1-4c1b-baf5-ded8157a4822/">here</a>. </p>-->
+	<!--  <p>Can&rsquo;t see the PDF? Click <a href="http://files.thetriangle.org/rate-cards/ratecard.pdf">here</a> to download a copy.</p>
+	<iframe style="width: 100%; height: 11in;" src="http://files.thetriangle.org/rate-cards/ratecard.pdf" frameborder="0" seamless></iframe> -->
 </div>
+	<p> The Triangle offers a number of print and digital advertising packages to suit your needs. View our 
+		rate card <a href="https://app.flytedesk.com/v3/shop/the-triangle-a3d66669/97b07f0a-4db1-4c1b-baf5-ded8157a4822/">here</a>. </p>
 
 <?php insert_ad('Global Banner Bottom', 'banner-bottom'); ?>
